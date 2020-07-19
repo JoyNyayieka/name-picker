d94eaa5a9dea76605c3f5b3562254b4329d8f9c0
JoyNyayieka <joynyayieka@gmail.com>
2020-07-19 22:05:37 +0300

    This commit will save changes made on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-
+var sunday=0
+var monday=1
+var tuesday=2
+var wednesday=3
+var thursday=4
+var friday=5
+var saturday=6
  if (weekday===0 && gender==="male"){
 alert("Your Akan Name is Kwasi!")
 alert("Your Akan Name is Kwasi!");
