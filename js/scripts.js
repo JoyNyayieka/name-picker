f89cb4731c625c50e955652cdc24dfed10eede04
JoyNyayieka <joynyayieka@gmail.com>
2020-07-28 20:56:17 +0300

    This commit will add the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,33 @@
-var weekday = function(CC, YY, MM, DD)
-{
-return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
-};
-if (DD>31 || DD<=0 ){
-alert("You entered an invalid date. Please try again.")
-}
-else if (MM>12 || MM<=0){
-alert("You entered an invalid month. Please try again.")
-}
-else if (weekday===0 && gender==="male"){
-alert("Your Akan Name is Kwasi!")
-alert("Your Akan Name is Kwasi!");
-}
-else if (weekday===1 && gender==="male"){
-alert("Your Akan Name is Kwadwo!")
-alert("Your Akan Name is Kwadwo!");
-}
-else if (weekday===2 && gender==="male"){
-alert("Your Akan Name is Kwabena!")
-alert("Your Akan Name is Kwabena!");
-}
-else if (weekday===3 && gender==="male"){
-alert("Your Akan Name is Kwaku!")
-alert("Your Akan Name is Kwaku!");
-}
-else if (weekday===4 && gender==="male"){
-alert("Your Akan Name is Yaw!")
-alert("Your Akan Name is Yaw!");
-}
-else if (weekday===5 && gender==="male"){
-alert("Your Akan Name is Kofi!")
-alert("Your Akan Name is Kofi!");
-}
-else if (weekday===6 && gender==="male"){
-alert("Your Akan Name is Kwame!")
-alert("Your Akan Name is Kwame!");
-}
-else if (weekday===0 && gender==="female"){
-alert("Your Akan Name is Akosua!")
-alert("Your Akan Name is Akosua!");
-}
-else if (weekday===1 && gender==="female"){
-alert("Your Akan Name is Adwoa!")
-alert("Your Akan Name is Adwoa!");
-}
-else if (weekday===2 && gender==="female"){
-alert("Your Akan Name is Abenaa!")
-alert("Your Akan Name is Abenaa!");
-}
-else if (weekday===3 && gender==="female"){
-alert("Your Akan Name is Akua!")
-alert("Your Akan Name is Akua!");
-}
-else if (weekday===4 && gender==="female"){
-alert("Your Akan Name is Yaa!")
-alert("Your Akan Name is Yaa!");
-}
-else if (weekday===5 && gender==="female"){
-alert("Your Akan Name is Afua!")
-alert("Your Akan Name is Afua!");
-}
-else if (weekday===6 && gender==="female"){
-alert("Your Akan Name is Ama!")
-alert("Your Akan Name is Ama!");
-}
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var dateComponents = []
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-alert(DD-MM-CCYY);
+function akanName() {
+  event.preventDefault();
+  var userBirthdate = document.getElementById("Date").value;
+  console.log(userBirthdate);
+  var gender = document.getElementById("userGender").value;
+  console.log(gender);
+  dateComponents = userBirthdate.split("-")
+  console.log(dateComponents);
+  var year = dateComponents[0];
+  var month = dateComponents[1]-1;
+  var date = dateComponents[2];
+  console.log(year);
+  console.log(month);
+  console.log(date);
+  var getDate = new Date(year, month, date);
+
+
+  var dateIndex = getDate.getDay();
+  console.log(getDate);
+  console.log(dateIndex);
+  console.log(maleNames[dateIndex]);
+  console.log(femaleNames[dateIndex]);
+  if (userGender = femaleNames) {
+
+  } else {userGender = maleNames
+  }
+  console.log()
+}
